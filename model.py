12908bd8878bb39003c029d9c851200d6aa25d6c
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2019-11-21 23:15:11 +0000

    separate

--- model.py
+++ model.py
@@ -448,8 +448,8 @@ class SR_Model(nn.Module):
 
 
         SRL_input = SRL_output.view(self.batch_size, seq_len, -1)
-        SRL_input = SRL_input.detach()
-        pred_recur = self.SR_Compressor(SRL_input.detach(), pretrain_emb,
+        SRL_input = SRL_input
+        pred_recur = self.SR_Compressor(SRL_input, pretrain_emb,
                                         flag_emb.detach(), word_id_emb, predicates_1D, seq_len, para=False)
 
         output_word = self.SR_Matcher(pred_recur, pretrain_emb, flag_emb.detach(), word_id_emb.detach(), seq_len, para=False)
