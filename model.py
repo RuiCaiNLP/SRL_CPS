32fc1d1e36d6dfd6dfc80de6b9780bcc5f668d65
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2019-12-28 13:55:27 +0000

    only transfer

--- model.py
+++ model.py
@@ -477,6 +477,8 @@ class SR_Model(nn.Module):
             bert_emb = bert_emb[torch.arange(bert_emb.size(0)).unsqueeze(-1), bert_out_positions].detach()
 
             for i in range(len(bert_emb)):
+                if i >= len(actual_lens):
+                    break
                 for j in range(len(bert_emb[i])):
                     if j >= actual_lens[i]:
                         bert_emb[i][j] = get_torch_variable_from_np(np.zeros(768, dtype="float32"))
