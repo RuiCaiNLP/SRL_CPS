2d3b0acbb57653903fb6bf3f548a442bd98dfdee
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2020-01-02 16:45:59 +0000

    para

--- model.py
+++ model.py
@@ -296,8 +296,8 @@ class SR_Model(nn.Module):
 
         output_word = self.SR_Matcher(pred_recur, pretrain_emb, flag_emb.detach(), None, seq_len,
                                       para=False, use_bert=True)
-        teacher = F.softmax(SRL_input, dim=2).detach()
-        student = F.log_softmax(output_word, dim=2)
+        teacher = F.softmax(output_SRL.detach(), dim=1).detach()
+        student = F.log_softmax(output_word, dim=1)
         unlabeled_loss_function = nn.KLDivLoss(reduction='none')
         loss = unlabeled_loss_function(student, teacher)
         loss = loss.sum() / (self.batch_size * seq_len)
