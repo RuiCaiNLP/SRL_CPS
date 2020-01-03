c7e0ffaadb7551c53cd940f0a7632ef112f7c378
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2020-01-03 19:07:36 +0000

    filter

--- model.py
+++ model.py
@@ -324,8 +324,9 @@ class SR_Model(nn.Module):
         return word_mask
 
     def word_mask(self,output_en_en, output_en_fr, seq_len_en, seq_len_fr):
-        word_mask_en = np.zeros((self.batch_size, seq_len_en), dtype="float32")
-        word_mask_fr = np.zeros((self.batch_size, seq_len_fr), dtype="float32")
+        word_mask_en = np.ones((self.batch_size, seq_len_en), dtype="float32")
+        word_mask_fr = np.ones((self.batch_size, seq_len_fr), dtype="float32")
+        return word_mask_en, word_mask_fr
         _, roles_en_fr = torch.max(output_en_fr, 2)
         _, roles_en_en = torch.max(output_en_en, 2)
         for i in range(self.batch_size):
