97b4bd1569a27b015a3ed9e11a8474000eafc615
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2020-01-25 23:11:21 +0000

    gaussion noise

--- model.py
+++ model.py
@@ -571,14 +571,14 @@ class SR_Model(nn.Module):
         #output_word_en_en = F.softmax(output_word_en_en, dim=1).detach()
         #output_word_fr_en = F.log_softmax(output_word_fr_en, dim=1)
         #loss = unlabeled_loss_function(output_word_fr_en, output_word_en_en)
-        output_word_en_en = F.softmax(max_enfr_en, dim=1).detach()
+        output_word_en_en = F.softmax(output_word_en_en, dim=1).detach()
         max_enfr_en = F.log_softmax(max_enfr_en, dim=1)
         loss = unlabeled_loss_function(max_enfr_en, output_word_en_en)
         loss = loss.sum(dim=1)#*word_mask_en_tensor
         loss = loss.sum() / (self.batch_size*seq_len_en)
 
         #output_word_en_fr = F.softmax(output_word_en_fr, dim=1).detach()
-        max_enfr_fr = F.softmax(output_word_fr_fr, dim=1).detach()
+        max_enfr_fr = F.softmax(max_enfr_fr, dim=1).detach()
         output_word_fr_fr = F.log_softmax(output_word_fr_fr, dim=1)
         loss_2 = unlabeled_loss_function(output_word_fr_fr, max_enfr_fr)
         loss_2 = loss_2.sum(dim=1)#*word_mask_fr_tensor
