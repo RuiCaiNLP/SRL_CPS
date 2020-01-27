1d254a12d99d8cee18b2bb24349b661a97e45d23
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2020-01-27 23:59:06 +0000

    copy

--- model_compress.py
+++ model_compress.py
@@ -862,15 +862,15 @@ class SR_Model(nn.Module):
             SRL_output = self.SR_Labeler(bert_emb, flag_emb, predicates_1D, seq_len, para=False, use_bert=True)
 
             SRL_input = SRL_output.view(self.batch_size, seq_len, -1)
-            #SRL_input_probs = F.softmax(SRL_input, 2).detach()
-            _, prediction_batch_variable = torch.max(SRL_input, 2)
+            SRL_input_probs = F.softmax(SRL_input, 2).detach()
+            #_, prediction_batch_variable = torch.max(SRL_input, 2)
             #print(prediction_batch_variable)
-            labeler_pred = np.zeros((self.batch_size, seq_len, self.target_vocab_size)).astype('float32')
-            for i in range(self.batch_size):
-                for j in range(seq_len):
-                    labeler_pred[i][j][prediction_batch_variable[i][j].data.cpu()] = 1.0
-            labeler_pred = get_torch_variable_from_np(labeler_pred).to(device)
-            pred_recur = self.SR_Compressor(labeler_pred, bert_emb,
+            #labeler_pred = np.zeros((self.batch_size, seq_len, self.target_vocab_size)).astype('float32')
+            #for i in range(self.batch_size):
+            #    for j in range(seq_len):
+            #        labeler_pred[i][j][prediction_batch_variable[i][j].data.cpu()] = 1.0
+            #labeler_pred = get_torch_variable_from_np(labeler_pred).to(device)
+            pred_recur = self.SR_Compressor(SRL_input_probs, bert_emb,
                                             flag_emb.detach(), word_id_emb, predicates_1D, seq_len, para=False,
                                             use_bert=True)
 
