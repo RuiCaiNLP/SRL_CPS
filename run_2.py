a5f8eb875311b2dd6f0c272b5161c73e1a04dc8b
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2020-01-28 10:33:23 +0000

    copy

--- run_2.py
+++ run_2.py
@@ -375,9 +375,9 @@ if __name__ == '__main__':
                 target_batch_variable = get_torch_variable_from_np(flat_argument)
 
                 out, out_word = srl_model(train_input_data, lang='En', use_bert=use_bert, isTrain=True)
-                _,  prediction_batch_variable = torch.max(out, 1)
+                #_,  prediction_batch_variable = torch.max(out, 1)
                 loss = criterion(out, target_batch_variable)
-                loss_word = criterion_word(out_word, prediction_batch_variable)
+                loss_word = criterion_word(out_word, target_batch_variable)
                 if batch_i % 50 == 0:
                     print("epoch:", epoch, batch_i, loss.item(), loss_word.item())
                 optimizer.zero_grad()
