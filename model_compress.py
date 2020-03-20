ee1555a5b536bd370f6961985e0355d1fa4584b7
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2020-03-20 09:22:59 +0000

    atten

--- model_compress.py
+++ model_compress.py
@@ -482,11 +482,11 @@ class SR_Model(nn.Module):
         seq_len = flag_emb.shape[1]
         SRL_output = self.SR_Labeler(bert_emb_en, flag_emb.detach(), predicates_1D, seq_len, para=True, use_bert=True)
 
-        CopyLoss_en = self.copy_loss(SRL_output, bert_emb_en_noise, flag_emb.detach(), seq_len)
+        CopyLoss_en = self.copy_loss(SRL_output, bert_emb_en, flag_emb.detach(), seq_len)
 
         SRL_input = SRL_output.view(self.batch_size, seq_len, -1)
         SRL_input = F.softmax(SRL_input, 2)
-        pred_recur = self.SR_Compressor(SRL_input.detach(), bert_emb_en,
+        pred_recur = self.SR_Compressor(SRL_input.detach(), bert_emb_en_noise,
                                         flag_emb.detach(), None, predicates_1D, seq_len, para=True, use_bert=True)
 
         seq_len_fr = flag_emb_fr.shape[1]
@@ -871,7 +871,7 @@ class SR_Model(nn.Module):
             SRL_input_probs = F.softmax(SRL_input, 2).detach()
 
             if isTrain:
-                pred_recur = self.SR_Compressor(SRL_input_probs, bert_emb_noise.detach(),
+                pred_recur = self.SR_Compressor(SRL_input_probs, bert_emb.detach(),
                                                 flag_emb.detach(), word_id_emb, predicates_1D, seq_len, para=False,
                                                 use_bert=True)
 
